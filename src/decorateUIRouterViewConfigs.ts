67be95b0a4f4815b515f9a82aa1fe2f26450e777
Justas Brazauskas <brazauskasjustas@gmail.com>
2020-08-17 17:17:40 +0300

    More proper view fix for dynamic react routes

--- src/decorateUIRouterViewConfigs.ts
+++ src/decorateUIRouterViewConfigs.ts
@@ -1,100 +1,93 @@
-import * as React from "react";
-import { hybridModule } from "./angularjs/module";
-import { PathNode, StateObject, UIRouter } from "@uirouter/core";
-import { ReactViewConfig, ReactViewDeclaration } from "@uirouter/react";
-
-/**
- * Registers a `react` view config factory which is invoked when `view.$type === 'react'`.
- *
- * Decorates the `views: {}` registered on states.
- * Detects if a `component:` is a React Component and sets `view.$type = 'react'` if so
- */
-
-const wrapperFactory = (view, cmp, originalType, originalComponent) =>
-  class WrapperComponent extends React.Component {
-    componentWillUnmount() {
-      if (view) {
-        // Revert back to original, unmodified type & component after this wrapper unmounts.
-        view.$type = originalType;
-        view.component = originalComponent;
-      }
-    }
-
-    render() {
-      return React.createElement(cmp, this.props, this.props.children);
-    }
-  };
+import * as React from 'react';
+import { hybridModule } from './angularjs/module';
+import { PathNode, StateObject, UIRouter } from '@uirouter/core';
+import { ReactViewConfig, ReactViewDeclaration } from '@uirouter/react';
 
 hybridModule.config([
-  "$uiRouterProvider",
+  '$uiRouterProvider',
   (router: UIRouter) => {
-    const factory = (path: [PathNode], config: ReactViewDeclaration) =>
-      new ReactViewConfig(path, config);
+    const factory = (path: [PathNode], config: ReactViewDeclaration) => new ReactViewConfig(path, config);
 
     // Add the react view config factory for react views
-    router.viewService._pluginapi._viewConfigFactory("react", factory);
+    router.viewService._pluginapi._viewConfigFactory('react', factory);
 
     // Decorate states at registration time with the view type
-    router.stateRegistry.decorator(
-      "views",
-      (state: StateObject, parentDecorator) => {
-        const views = parentDecorator(state);
+    router.stateRegistry.decorator('views', (state: StateObject, parentDecorator) => {
+      const views = parentDecorator(state);
 
-        const self = state.self;
-        const selfViews = self.views || { $default: self };
-        const isReactComponent = cmp =>
-          cmp instanceof React.Component ||
-          (cmp && cmp.prototype && cmp.prototype.isReactComponent) ||
-          (cmp &&
-            cmp.prototype &&
-            typeof cmp.prototype.render === "function") ||
-          (cmp && typeof cmp === "function");
+      const self = state.self;
+      const selfViews = self.views || { $default: self };
+      const isReactComponent = cmp =>
+        cmp instanceof React.Component ||
+        (cmp && cmp.prototype && cmp.prototype.isReactComponent) ||
+        (cmp && cmp.prototype && typeof cmp.prototype.render === 'function') ||
+        (cmp && typeof cmp === 'function');
 
-        Object.keys(views).forEach(key => {
-          const view = views[key];
-          if (view.componentProvider) {
-            // If view component provider is given, monkey-patch the type in runtime.
-            const injectables = [...view.componentProvider];
-            // Remove the runner function, and just leave `injectables` with deps.
-            const func = injectables.pop();
+      Object.keys(views).forEach(key => {
+        const view = views[key];
+        if (view.componentProvider) {
+          // If view component provider is given, monkey-patch the type in runtime.
+          const injectables = [...view.componentProvider];
+          // Remove the runner function, and just leave `injectables` with deps.
+          const func = injectables.pop();
 
-            // Create a new component provider.
-            view.componentProvider = [
-              // Original injectables.
-              ...injectables,
-              (...args) => {
-                // Get the component declaration.
-                const cmp = func(...args);
-                // Determine if it's react.
-                var reactType = isReactComponent(cmp) && "react";
-                // Preserve the original type & component.
-                const originalType = view.$type;
-                const originalComponent = view.component;
-                view.$type = selfViews[key].$type || reactType || view.$type;
+          const originalType = view.$type;
+          const originalComponent = view.component;
+          const originalExit = view.$context.onExit;
+          // Once the view exits, reset the view type, so `componentProvider` runs again.
+          if (!originalExit || !originalExit.$$__ui_router_react_view_fixer) {
+            const originalExitDeps = [...(originalExit || [])];
+            const originalExitFn = originalExitDeps.pop();
 
-                if (view.$type === "react") {
-                  // Set the component, if it's react, as `ReactViewConfig` expects the `component`
-                  // property.
-                  view.component = wrapperFactory(
-                    view,
-                    cmp,
-                    originalType,
-                    originalComponent
-                  );
+            view.$context.onExit = [
+              ...originalExitDeps,
+              function(...args) {
+                if (view.$type !== originalType) {
+                  view.$type = originalType;
                 }
-                // Also return component for AngularjS components.
-                return cmp;
-              }
-            ];
-          } else {
-            var selfView = selfViews[key || "$default"];
-            var reactType = isReactComponent(view.component) && "react";
-            view.$type = selfViews[key].$type || reactType || view.$type;
-          }
-        });
 
-        return views;
-      }
-    );
-  }
+                if (view.component !== originalComponent) {
+                  view.component = originalComponent;
+                }
+
+                if (typeof originalExitFn === 'function') {
+                  return originalExitFn(...args);
+                }
+              },
+            ];
+
+            // Only decorate the onExit once per state declaration.
+            view.$context.onExit.$$__ui_router_react_view_fixer = true;
+          }
+
+          // Create a new component provider.
+          view.componentProvider = [
+            // Original injectables.
+            ...injectables,
+            (...args) => {
+              // Get the component declaration.
+              const cmp = func(...args);
+              // Determine if it's react.
+              var reactType = isReactComponent(cmp) && 'react';
+              view.$type = selfViews[key].$type || reactType || view.$type;
+
+              if (view.$type === 'react') {
+                // Set the component, if it's react, as `ReactViewConfig` expects the `component`
+                // property.
+                view.component = cmp;
+              }
+              // Also return component for AngularjS components.
+              return cmp;
+            },
+          ];
+        } else {
+          var selfView = selfViews[key || '$default'];
+          var reactType = isReactComponent(view.component) && 'react';
+          view.$type = selfViews[key].$type || reactType || view.$type;
+        }
+      });
+
+      return views;
+    });
+  },
 ]);
